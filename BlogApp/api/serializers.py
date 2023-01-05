c9a4acf8c3f1e8351c6d5970cf877fc21c8a6cd1
JC <chohy1106@gmail.com>
2023-01-05 22:59:33 +0900

    DRF2

--- BlogApp/api/serializers.py
+++ BlogApp/api/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers
-from models import Post
-from django.contrib.auth.models import User
-
-class UserSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email')
+from BlogApp.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    user = UserSerializers(read_only = True)
     class Meta:
         model = Post
         fields = (
--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,10 +1,55 @@
 from django.shortcuts import render
 from .api.serializers import PostSerializer
-from rest_framework.response import responses
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
+# APIView 공통적인 함수 재사용 가능, DRY 정책을 따르도록 하는 강력한 패턴 제공
+from rest_framework.view import APIView
 from .models import Post
 from django.http import Http404
 from rest_framework import status
+
+class PostList(APIView):
+    def get(self, request, format=None):
+        post = Post.objects.all()
+        serializer = PostSerializer(post, many=True)
+        return Response(serializer.data) 
+
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_201_CREATED)
+
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
+
+    
+
 
 @api_view(['GET', 'POST'])
 def PostList(request):
@@ -16,7 +61,7 @@ def PostList(request):
         # serializer은 한개의 객체만 이해할수 있고, 리스트는 이해할 수 없다.
         # 따라서 many=True를 추가해 중복 표현 값에 대한 list를 받게끔 해주는 것이다.
 
-        return responses(serializer.data)
+        return Response(serializer.data)
         # Response의 정체???
         # TemplateResponse 객체의 일종으로 렌더링되지 않은 컨텐츠를 가져오고
         # 클라이언트에게 반환할 올바른 컨텐츠 유형을 결정한다!
